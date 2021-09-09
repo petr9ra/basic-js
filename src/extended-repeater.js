a9eff66efc90cced6675198191801c336d2338eb
petr9ra <petr9ra@yandex.ru>
2021-09-09 23:29:23 +0300

    feat: add solution for extended-repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,7 +15,32 @@ import { NotImplementedError } from '../extensions/index.js';
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function repeater(str, options) {
+  let result = [];
+  str = String(str)
+  if(options.addition || typeof options.addition === 'boolean' || options.addition === null) options.addition = String(options.addition)
+
+  if (options.repeatTimes) {
+    for (let i = 0; i < options.repeatTimes; i++) {
+      result.push(str)
+
+      if (options.addition) {
+        if (options.additionRepeatTimes) {
+          for (let j = 0; j < options.additionRepeatTimes; j++) {
+            result.push(options.addition)
+            if (options.additionSeparator && j < options.additionRepeatTimes - 1) result.push(options.additionSeparator)
+            else if (!options.additionSeparator && j < options.additionRepeatTimes - 1) result.push('|')
+          }
+        } else result.push(options.addition)
+      } 
+
+      if (options.separator && i < options.repeatTimes - 1) result.push(options.separator)
+      else if (!options.separator && i < options.repeatTimes - 1) result.push('+')
+    }
+    
+  } else {
+    result.push(str)
+    if (options.addition) result.push(options.addition)
+  } 
+  return result.join('');
 }
