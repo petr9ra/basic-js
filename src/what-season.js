3195193191a817532d1ad627d67a42369c95cf38
Petr <petr9ra@yandex.ru>
2021-09-12 23:43:58 +0300

    fix: tricky moment in what-season task

--- src/what-season.js
+++ src/what-season.js
@@ -13,7 +13,11 @@ import { NotImplementedError } from '../extensions/index.js';
  */
 export default function getSeason(date) {
   if (!date) return 'Unable to determine the time of year!';
-  if (!date.getMonth) throw new Error('Invalid date!');
+  try {
+    date.getTime();
+  } catch (error) {
+    throw new Error ('Invalid date!');
+  }
   let month = date.getMonth();
   if (month >= 2 && month <= 4) return 'spring';
   else if (month >= 5 && month <= 7) return 'summer';
